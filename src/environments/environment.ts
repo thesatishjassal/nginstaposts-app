9f7dfd31096a1bf308d2282c829109a11520bdaf
codewithsatish <codewithsatish@gmail.com>
2021-02-27 13:33:13 +0530

    removed firebase details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBR8MhC9wxoZJE0UTCqVKtZpRQdN_5ekyQ',
-    authDomain: 'githubf-pro.firebaseapp.com',
-    databaseURL: 'https://githubf-pro-default-rtdb.firebaseio.com',
-    projectId: 'githubf-pro',
-    storageBucket: 'githubf-pro.appspot.com',
-    messagingSenderId: '946543545710',
-    appId: '1:946543545710:web:bc31f3465c2ce24442457f',
-  },
 };
 
 /*
